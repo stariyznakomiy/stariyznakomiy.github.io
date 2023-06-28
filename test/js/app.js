b638cbe92841e8f9684a2ff2541ced4966528737
stariyznakomiy <stariyznakomiy0@gmail.com>
2023-06-28 23:56:08 +0300

    new

--- test/js/app.js
+++ test/js/app.js
@@ -1,18 +1,5 @@
 (() => {
     "use strict";
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(webP.height == 2);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = support === true ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     let bodyLockStatus = true;
     let bodyLockToggle = (delay = 500) => {
         if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
@@ -59,16 +46,6 @@
             }
         }));
     }
-    let addWindowScrollEvent = false;
-    setTimeout((() => {
-        if (addWindowScrollEvent) {
-            let windowScroll = new Event("windowScroll");
-            window.addEventListener("scroll", (function(e) {
-                document.dispatchEvent(windowScroll);
-            }));
-        }
-    }), 0);
     window["FLS"] = true;
-    isWebp();
     menuInit();
 })();
